8f3bfad5caf56c71c330c4de0772b84079421f63
Cong Phan <congthundergodphan@gmail.com>
2023-09-18 18:16:46 +0700

    [B] Debug

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -3,20 +3,31 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-    logging: false,
-  }
-);
+const sequelize = process.env.DB_EXTERNAL_URL
+  ? new Sequelize(process.env.DB_EXTERNAL_URL, {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000,
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000,
+        },
+        logging: false,
+      }
+    );
 
 export default sequelize;
