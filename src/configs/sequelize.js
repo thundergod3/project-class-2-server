e2c94b07aacd78ac318de32884a23eee03959a9b
Cong Phan <congthundergodphan@gmail.com>
2024-06-05 21:43:17 +0700

    [B] Fixing build failed

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -3,24 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const sequelize =
-  process.env.NODE_ENV === "development"
-    ? new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,
-        {
-          host: process.env.DB_HOST,
-          dialect: "postgres",
-          pool: {
-            max: 5,
-            min: 0,
-            idle: 10000,
-          },
-          logging: false,
-        }
-      )
-    : new Sequelize(process.env.DB_EXTERNAL_URL, {
+const sequelize = process.env.DB_EXTERNAL_URL
+  ? new Sequelize(process.env.DB_EXTERNAL_URL, {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000,
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
         host: process.env.DB_HOST,
         dialect: "postgres",
         pool: {
@@ -29,6 +27,7 @@ const sequelize =
           idle: 10000,
         },
         logging: false,
-      });
+      }
+    );
 
 export default sequelize;
